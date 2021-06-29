c7d952ff39167724326936a81cce618834c264e2
cccbeginner <arvin.tsai.0113@gmail.com>
2021-06-29 11:00:50 +0800

    add viewModel factory

--- app/src/main/java/com/example/moneymanager/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/moneymanager/ui/home/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.example.moneymanager.MyApplication
@@ -22,7 +23,7 @@ class HomeFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         val userRepository = (requireActivity().application as MyApplication).userRepository
-        homeViewModel = HomeViewModel(userRepository)
+        homeViewModel = ViewModelProvider(this, HomeViewModelFactory(userRepository)).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
 
         val textUsername: TextView = root.findViewById(R.id.home_username)
--- app/src/main/java/com/example/moneymanager/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/moneymanager/ui/home/HomeViewModel.kt
@@ -1,10 +1,17 @@
 package com.example.moneymanager.ui.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.asLiveData
 import com.example.moneymanager.repository.UserRepository
 
 class HomeViewModel(userRepository: UserRepository) : ViewModel() {
 
     val user = userRepository.currentUserFlow.asLiveData()
+}
+
+class HomeViewModelFactory(private val userRepository: UserRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return HomeViewModel(userRepository) as T
+    }
 }
